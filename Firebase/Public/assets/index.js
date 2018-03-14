b36850cea510be6dbcf5848669b826573fb88639
Monir Cederstrom <monircederstrom@bootcamp.attlocal.net>
2018-03-14 13:37:09 -0500

    add javascript code with out firebase

--- Firebase/Public/assets/index.js
+++ Firebase/Public/assets/index.js
@@ -1,4 +1,4 @@
-  // Initialize Firebase
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyCxAPllmMn1a0Xc1V7GCSVW6V_TroVw4Xk",
     authDomain: "bootcamp-cc0b8.firebaseapp.com",
@@ -8,13 +8,44 @@
     messagingSenderId: "319783617182"
   };
   firebase.initializeApp(config);
-  console.log("working!")
-  $(function() {
-    let database = firebase.database();
-    $("form").on("submit", function (e) {
-      e.preventDefault();
-      let val = $(".title-input").val();
-      database.ref("value").set(val);
-    })
-    database.ref("message");
-  })
+  var database = firebase.database();
+    
+    //function for submit button
+    $("#add-train-btn").on("click", function (event) {
+      event.preventDefault();
+    
+    //set variables for user input
+    var trainName = $("#name").val().trim();
+    var trainDestination = $("#destination").val().trim();
+    var trainStart = moment($("#start").val().trim(,) "HH:mm").forma("X");
+    var trainFreq = $("#frequency").val().trim();
+    
+    //create temporary object to story input of train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        start: trainStart,
+        frequency: trainFreq,
+    //End of newTrain object 
+    }
+   
+    //push train object to database
+    database.ref().push(newTrain);
+
+    //Check object in console.log
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
+
+    //Clear out form text boxes after submit button is clicked
+    $("#name").val("");
+    $("#destination").val("");
+    $("#start").val("");
+    $("#frequency").val("");
+//End of submit button function
+})
+
+
+//end of initialize firebase function
+}
